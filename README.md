b1fb95213ab39e6585c43a66a62dd068592854bc
Vaughn Friesen <vdfriesen@vaughnfriesen.com>
2016-12-24 12:33:24 +0800

    Add config file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Go Redis Types
+# Redis Types demo in Golang
 
-[![GoDoc](https://godoc.org/github.com/MasterOfBinary/redistypes?status.svg)](https://godoc.org/github.com/MasterOfBinary/redistypes)
 [![Build Status](https://travis-ci.org/MasterOfBinary/goredistypes.svg?branch=master)](https://travis-ci.org/MasterOfBinary/goredistypes)
+[![GoDoc](https://godoc.org/github.com/MasterOfBinary/redistypes?status.svg)](https://godoc.org/github.com/MasterOfBinary/redistypes)
 
 A simple program demonstrating the functionality of the Redis data types library https://github.com/MasterOfBinary/redistypes.
--- main.go
+++ main.go
@@ -5,12 +5,39 @@ import (
 	"net"
 	"time"
 
+	"encoding/json"
+	"io/ioutil"
+
 	"github.com/MasterOfBinary/redistypes"
 	"github.com/garyburd/redigo/redis"
 )
 
+type config struct {
+	HostAndPort string `json:"hostAndPort"`
+}
+
+var defaultConfig = config{"127.0.0.1:6379"}
+
+func loadConfig() config {
+	bytes, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		fmt.Errorf("Falling back to default config")
+		return defaultConfig
+	}
+
+	var c config
+	err = json.Unmarshal(bytes, &c)
+	if err != nil {
+		fmt.Errorf("Falling back to default config")
+		return defaultConfig
+	}
+
+	return c
+}
+
 func main() {
-	netConn, err := net.Dial("tcp", "192.168.1.109:6379")
+	conf := loadConfig()
+	netConn, err := net.Dial("tcp", conf.HostAndPort)
 	if err != nil {
 		panic(err)
 	}
